36c1bf3bb07d07ae30f75605e89a1e84294ea6b2
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-17 13:21:27 +0200

    secondcommit

--- ejercicioUsuario.js
+++ ejercicioUsuario.js
@@ -18,4 +18,27 @@ const api = fetch('https://fakerapi.it/api/v1/users?_quantity=20&_gender=male')
 
 
 
-//3
+//3 Crear un input que permita filtrar por nombre de la persona
+
+import { personas } from 'personas.js';
+
+const inputName = document.getElementById('inputName');
+inputName.placeholder = 'Busqueda por nombre y apellidos';
+const input = document.createElement('input');
+input.addEventListener("input", (event) => {
+    const userInput = event.target.value.toLowerCase();
+    
+    // Filtrar las personas por nombre y apellidos
+    const personasFiltradas = personas.filter(persona =>
+        persona.firstname.toLowerCase().includes(userInput) || 
+        persona.lastname.toLowerCase().includes(userInput)
+    );
+    
+    // Llamar a una función para escribir los resultados en el HTML
+    writeInputInHtml(personasFiltradas);
+});
+
+
+
+inputName.appendChild(input);
+
--- usuarios.js
+++ usuarios.js
@@ -14,6 +14,15 @@ class Usuario extends Personas {
 
 const usuarios = document.getElementById("usuarios");
 
-const usuario = document.createElement('p');
-usuario.textContent = `Nombre: ${usuario.firstname} ${usuario.lastname}, Username: ${usuario.username}, Email: ${usuario.email}`;
-usuarios.appendChild(usuario);
+
+const usuario1 = new Usuario('Lucia', 'Macho', 21, 'lulums', 'luciamacho00@gmail.com');
+const usuario2 = new Usuario('Carlota', 'Casas', 22, 'carloti', 'carlota20@gmail.com');
+
+
+const usuario1Element = document.createElement('usuario1Element');
+usuario1Element.textContent = `Nombre: ${usuario1.firstname} ${usuario1.lastname}, Username: ${usuario1.username}, Email: ${usuario1.email}`;
+usuarios.appendChild(usuario1Element);
+
+const usuario2Element = document.createElement('usuario2Element');
+usuario2Element.textContent = `Nombre: ${usuario2.firstname} ${usuario2.lastname}, Username: ${usuario2.username}, Email: ${usuario2.email}`;
+usuarios.appendChild(usuario2Element);
